cf63c190bbb7853c28381dbe898db6f648e544ec
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-05 16:00:49 +0000

    working solution but still comma at end

--- dice-roller.js
+++ dice-roller.js
@@ -1,11 +1,15 @@
 //Capture 1 Parameter in command line
 var userInput = process.argv[2];
-console.log(userInput);
 //Create a function for dice roll
 function diceRoll(numberOfDice) {
-
+  //Use parameter for number of dice to roll
+  //Loop through the number provided
+  var result = "";
+  for (var i = 1; i <= numberOfDice; i++) {
+    result += Math.floor(Math.random() * 6 + 1) + ", ";
+  }
+  return result;
+  //Use Math.random multiplied by 6 for dice roll
+  //Output "Rolled (number) dice: (generated numbers)"
 }
-//Use parameter for number of dice to roll
-//Loop through the number provided
-//Use Math.random multiplied by 6 for dice roll
-//Output "Rolled (number) dice: (generated numbers)"
+console.log("Rolled " + userInput + " dice: " + diceRoll(userInput));
