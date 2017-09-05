4e2c69c08e011950e00713d6119c1302bc0d9aa0
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-05 15:13:34 +0000

    Added input capture and initial function

--- dice-roller.js
+++ dice-roller.js
@@ -1,5 +1,10 @@
-//Create a function for dice roll
 //Capture 1 Parameter in command line
+var userInput = process.argv[2];
+console.log(userInput);
+//Create a function for dice roll
+function diceRoll(numberOfDice) {
+
+}
 //Use parameter for number of dice to roll
 //Loop through the number provided
 //Use Math.random multiplied by 6 for dice roll
